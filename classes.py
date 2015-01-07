3d25299c4dd77bf1cb5ada0cf2fb2ef95dd31ac1
Appolloyon <cklinger@ualberta.ca>
2015-01-07 09:28:42 -0700

    Modified total_editing.py to calculate codon prefs.
    Codon preferences are calculated simply as the number of times each codon shows
    up in the genomic and mRNA sequence. Sequence starts and stops as before
    (compares only the aligned regions), and it is ensured that each codon is counted
    only once by recording codons only when codon_pos is 3. Missing data at the
    beginning of the mRNA sequence is compensated by ensuring that only full
    codons are counted for the first codon recorded. The original SeqPair class
    in classes.py was copied and slightly modified to yield CodonPair, which is
    present in the same file.

--- classes.py
+++ classes.py
@@ -106,3 +106,150 @@ class SeqPair(object):
                     return k1
 
 
+class CodonPair(object):
+    """Class to model DNA/mRNA sequence pairs"""
+
+    def __init__(self, mseq, gseq, name):
+        self.name = name
+        self.mseq = mseq
+        self.gseq = gseq
+        self.gnuc_index = 0  #initialize counter for index in gen sequence
+        self.mnuc_index = 0  #initialize counter for index in mRNA sequence
+        self.codon_pos = 1 #keep track of position in codon
+        self.gnuc_aa_dict = {
+            'F':{'TTT':0,'TTC':0},
+            'L':{'TTA':0,'TTG':0,'CTT':0,'CTC':0,'CTA':0,'CTG':0},
+            'I':{'ATT':0,'ATC':0,'ATA':0},
+            'M':{'ATG':0},
+            'V':{'GTT':0,'GTC':0,'GTA':0,'GTG':0},
+            'S':{'TCT':0,'TCC':0,'TCA':0,'TCG':0,'AGT':0,'AGC':0},
+            'P':{'CCT':0,'CCC':0,'CCA':0,'CCG':0},
+            'T':{'ACT':0,'ACC':0,'ACA':0,'ACG':0},
+            'A':{'GCT':0,'GCC':0,'GCA':0,'GCG':0},
+            'Y':{'TAT':0,'TAC':0},
+            'H':{'CAT':0,'CAC':0},
+            'Q':{'CAA':0,'CAG':0},
+            'N':{'AAT':0,'AAC':0},
+            'K':{'AAA':0,'AAG':0},
+            'D':{'GAT':0,'GAC':0},
+            'E':{'GAA':0,'GAG':0},
+            'C':{'TGT':0,'TGC':0},
+            'W':{'TGG':0},
+            'R':{'CGT':0,'CGC':0,'CGA':0,'CGG':0,'AGA':0,'AGG':0},
+            'G':{'GGT':0,'GGC':0,'GGA':0,'GGG':0},
+            'STOP':{'TAA':0,'TAG':0,'TGA':0}
+            }
+        self.mnuc_aa_dict = {
+            'F':{'TTT':0,'TTC':0},
+            'L':{'TTA':0,'TTG':0,'CTT':0,'CTC':0,'CTA':0,'CTG':0},
+            'I':{'ATT':0,'ATC':0,'ATA':0},
+            'M':{'ATG':0},
+            'V':{'GTT':0,'GTC':0,'GTA':0,'GTG':0},
+            'S':{'TCT':0,'TCC':0,'TCA':0,'TCG':0,'AGT':0,'AGC':0},
+            'P':{'CCT':0,'CCC':0,'CCA':0,'CCG':0},
+            'T':{'ACT':0,'ACC':0,'ACA':0,'ACG':0},
+            'A':{'GCT':0,'GCC':0,'GCA':0,'GCG':0},
+            'Y':{'TAT':0,'TAC':0},
+            'H':{'CAT':0,'CAC':0},
+            'Q':{'CAA':0,'CAG':0},
+            'N':{'AAT':0,'AAC':0},
+            'K':{'AAA':0,'AAG':0},
+            'D':{'GAT':0,'GAC':0},
+            'E':{'GAA':0,'GAG':0},
+            'C':{'TGT':0,'TGC':0},
+            'W':{'TGG':0},
+            'R':{'CGT':0,'CGC':0,'CGA':0,'CGG':0,'AGA':0,'AGG':0},
+            'G':{'GGT':0,'GGC':0,'GGA':0,'GGG':0},
+            'STOP':{'TAA':0,'TAG':0,'TGA':0}
+            }
+
+
+    def index_nuc(self):
+        return self.gnuc_index
+
+    def index_mrna(self):
+        return self.mnuc_index
+
+    def index_position(self):
+        return self.codon_pos
+
+    def incr_nuc(self):
+        self.gnuc_index += 1
+        #print self.gnuc_index
+
+    def incr_mrna(self):
+        self.mnuc_index += 1
+        #print self.mnuc_index
+
+    def incr_pos(self):
+        """maintains position within a codon as 1, 2, or 3"""
+        if self.codon_pos < 3:
+            self.codon_pos += 1
+        else:
+            self.codon_pos = 1
+
+    def incr_all(self):
+        """advances counters through DNA only"""
+        self.incr_pos()
+        self.incr_nuc()
+
+    def lookup_gnuc(self):
+        return self.gseq[self.gnuc_index]
+
+    def lookup_mnuc(self):
+        return self.mseq[self.mnuc_index]
+
+    def lookup_gcodon(self):
+        """returns DNA index as a codon"""
+        if self.codon_pos == 1:  #uses codon position to determine slice
+            return self.gseq[self.index_nuc():(self.index_nuc()+3)]
+        elif self.codon_pos == 2:
+            return self.gseq[(self.index_nuc()-1):(self.index_nuc()+2)]
+        else:
+            return self.gseq[(self.index_nuc()-2):self.index_nuc()+1]
+
+    def lookup_mcodon(self):
+        """returns mRNA index as a codon"""
+        if self.codon_pos == 1:
+            return self.mseq[self.index_mrna():(self.index_mrna()+3)]
+        elif self.codon_pos == 2:
+            return self.mseq[(self.index_mrna()-1):(self.index_mrna()+2)]
+        else:
+            return self.mseq[(self.index_mrna()-2):self.index_mrna()+1]
+
+    def lookup_gaa(self):
+        """returns aa specified by DNA codon"""
+        codon = self.lookup_gcodon()
+        #print codon
+        for k1 in self.aa_dict.keys():
+            for e in self.aa_dict.get(k1):
+                if e == codon:
+                    return k1
+
+    def lookup_maa(self):
+        """returns aa specified by mRNA codon"""
+        codon = self.lookup_mcodon()
+        #print codon
+        for k1 in self.aa_dict.keys():
+            for e in self.aa_dict.get(k1):
+                if e == codon:
+                    return k1
+
+    def update_gcodons(self):
+        """updates dict based on codon retrieved"""
+        gcodon = self.lookup_gcodon()
+        #print codon
+        for k1 in self.gnuc_aa_dict.keys():
+            for k2 in self.gnuc_aa_dict[k1].keys():
+                if k2 == gcodon:
+                    self.gnuc_aa_dict[k1][k2] += 1
+
+    def update_mcodons(self):
+        """updates dict based on codon retrieved"""
+        mcodon = self.lookup_mcodon()
+        #print codon
+        for k1 in self.mnuc_aa_dict.keys():
+            for k2 in self.mnuc_aa_dict[k1].keys():
+                if k2 == mcodon:
+                    self.mnuc_aa_dict[k1][k2] += 1
+
---
+++ gene_codon_pref.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+"""
+Changelog
+---------
+Author: Christen Klinger
+Created: January 6, 2015
+Last Updated: January 6, 2015
+"""
+
+import re
+import argparse
+from classes import CodonPair
+
+parser = argparse.ArgumentParser(
+    description = """Calculates codon preference between genomic/RNA sequences""",
+    epilog = """This program assumes that the genomic and RNA sequence for a
+    given gene are provided in the same file in aligned FASTA format (.afa). It
+    also assumes that the files have the string 'gen' or 'mrna' in the filename,
+    for genomic and mRNA sequence, respectively. Output is given as a series of
+    csv values for various pertinent stats.""")
+parser.add_argument('infiles', nargs='+', help='list of aligned infiles')
+args = parser.parse_args()
+
+def gulp(string, start, gulp_size):
+    """get substrings of a string"""
+    gulpstr = ''
+    chars = string[start:start+gulp_size]
+    for char in chars:
+        gulpstr += char
+    return gulpstr
+
+def compare_seqs(seq1, seq2):
+    """compare substrings to determine start of alignment"""
+    equal = 0
+    for i, (r1, r2) in enumerate(zip(seq1, seq2)):
+        if i == 0:  #terminal residue
+            if r1 != '-' and r2 != '-':  #neither should be a gap
+                if r1 == r2:
+                    equal += 1
+                else:
+                    pass
+            else:
+                return False
+        else:  #other residues
+            if r1 == r2:
+                equal += 1
+            else:
+                pass
+    if equal >= 7:  #arbitrary threshold
+        return True
+    else:
+        return False
+
+def nonblank_lines(f):
+    """skip blank lines"""
+    for l in f:
+        line = l.strip('\n')
+        if line:
+            yield line
+
+def sanitize(seq):
+    """remove gap characters"""
+    nseq = ''
+    for char in seq:
+        if char == '-':
+            pass
+        else:
+            nseq += char
+    return nseq
+
+
+for infile in args.infiles:
+    name = infile.strip('.afa')
+    with open(infile,'U') as f:
+        seqdict={}
+        for line in nonblank_lines(f):
+            line = line.strip('\n')
+            if line.startswith(">"):
+                line = line.strip(">")
+                ID = line
+                seqdict[ID] = ''
+            else:
+                seqdict[ID] += line
+
+    for k in seqdict.keys():
+        if re.search('mrna',k) or re.search('mRNA',k) or re.search('mRNAs',k):
+            mseq = seqdict.get(k)
+        else:
+            gseq = seqdict.get(k)
+
+    smseq = sanitize(mseq)
+    sgseq = sanitize(gseq)
+    #print "smseq"
+    #print smseq
+    #print "sgseq"
+    #print sgseq
+
+    seq_pair = CodonPair(smseq, sgseq, name)  #sanitized sequences for direct comparison
+
+    i = 0
+    while not compare_seqs((gulp(mseq, i, 9)), (gulp(gseq, i, 9))):  #start of alignment
+        if gseq[i] != '-':
+            seq_pair.incr_all()
+        if mseq[i] != '-':
+            seq_pair.incr_mrna()
+        i += 1
+    j = 0
+    while not compare_seqs((gulp(mseq[::-1], j, 9)), (gulp(gseq[::-1], j, 9))):  #end of alignment
+        j += 1
+
+    newmseq = mseq[i:(len(mseq)-j)]  #only compare regions that align
+    newgseq = gseq[i:(len(gseq)-j)]
+    #print "newmseq"
+    #print newmseq
+    #print "newgseq"
+    #print newgseq
+
+    for i, (rg, rm) in enumerate(zip(newgseq, newmseq)):  #compare matching regions
+        #print i
+        #print "codon position" + '' + str(seq_pair.codon_pos)
+        if seq_pair.codon_pos != 3 or i < 2:
+            seq_pair.incr_all()
+            seq_pair.incr_mrna()
+        else:
+            if rg == '-' and rm != '-':  #insertion in mRNA
+                seq_pair.incr_mrna()
+            elif rm == '-' and rg != '-':  #insertion in DNA
+                seq_pair.incr_all()
+            else:  #residue in both
+                #print "gcodon:" + str(seq_pair.lookup_gcodon())
+                #print "mcodon:" + str(seq_pair.lookup_mcodon())
+                seq_pair.update_gcodons()
+                seq_pair.update_mcodons()
+                seq_pair.incr_all()
+                seq_pair.incr_mrna()
+
+    #for (k1,v1), (k2,v2) in zip(seq_pair.gnuc_aa_dict.items(), seq_pair.mnuc_aa_dict.items()):
+    #    print k1, v1
+    #    print k2, v2
+
+    out = name + "_codons.csv"
+    with open(out,'w') as o:
+        o.write("amino acid,codon,genome usage,mRNA usage")
+        o.write("\n")
+        for k1, k2 in zip(seq_pair.gnuc_aa_dict, seq_pair.mnuc_aa_dict):
+            o.write(k1)
+            for k3, k4 in zip(seq_pair.gnuc_aa_dict[k1].keys(), seq_pair.mnuc_aa_dict[k2].keys()):
+                o.write(',' + k3 + ',' + str(seq_pair.gnuc_aa_dict[k1][k3]) + ',' + str(seq_pair.mnuc_aa_dict[k1][k4]) + "\n")
+            o.write("\n")
