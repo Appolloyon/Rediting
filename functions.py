9d6323c4f6aa0d54e1412a1f5f18fcf1b7f91493
Appolloyon <cklinger@ualberta.ca>
2015-05-11 13:39:10 -0600

    Moved functions shared between editing and indel programs to separate file

---
+++ functions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+def gulp(string, start, gulp_size):
+    """get substrings of a string"""
+    gulpstr = ''
+    chars = string[start:start+gulp_size]
+    for char in chars:
+        gulpstr += char
+    return gulpstr
+
+def compare_seqs(seq1, seq2):
+    """compare substrings to determine start of alignment"""
+    equal = 0
+    for i, (r1, r2) in enumerate(zip(seq1, seq2)):
+        if i == 0:  #terminal residue
+            if r1 != '-' and r2 != '-':  #neither should be a gap
+                if r1 == r2:
+                    equal += 1
+                else:
+                    pass
+            else:
+                return False
+        else:  #other residues
+            if r1 == r2:
+                equal += 1
+            else:
+                pass
+    if equal >= 7:  #arbitrary threshold
+        return True
+    else:
+        return False
+
+def nonblank_lines(f):
+    """skip blank lines"""
+    for l in f:
+        line = l.strip('\n')
+        if line:
+            yield line
+
+def sanitize(seq):
+    """remove gap characters"""
+    nseq = ''
+    for char in seq:
+        if char == '-':
+            pass
+        else:
+            nseq += char
+    return nseq
+
+
