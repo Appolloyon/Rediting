7d5406f7f137e907ee4eb4eb8554b9c96493de17
Appolloyon <cklinger@ualberta.ca>
2016-03-08 08:31:25 -0700

    Missed one error from before, need to test again.

--- rediting/sequence_trimmer.py
+++ rediting/sequence_trimmer.py
@@ -23,11 +23,17 @@ parser.add_argument('-r', '--RNA', help='unique string present in RNA sequence h
 parser.add_argument('infiles', nargs='+', help='list of infiles')
 args = parser.parse_args()
 
+# Unlike other programs in this package, this one is written to be used
+# without a wrapper script, but could be easily adapted to do so
 for infile in args.infiles:
-    basename = infile.rsplit('.',1)[0] #everything before final period
+    # Gets the basename for the file
+    basename = infile.rsplit('.',1)[0]
+    # We actually provide aligned and sequence-only
+    # versions of the output
     out_align = basename + "_trimmed.afa"
     out_seq = basename + "_trimmed.fa"
 
+    # Load sequence data into a data structure for internal use
     seqdict = {}
     files.build_seqdict(infile,seqdict)
 
@@ -36,6 +42,8 @@ for infile in args.infiles:
     # Sequences must be in upper case
     for k in seqdict.keys():
         if re.search(rna_string,k):
+            # Since we are writing these data back out again
+            # we want to keep track of sequence headers
             rna_header = k
             rna_seq = seqdict.get(k).upper()
         elif re.search(gen_string,k):
@@ -47,36 +55,51 @@ for infile in args.infiles:
 
     san_gen_seq = strings.sanitize(gen_seq)
     san_ref_seq = strings.sanitize(ref_seq)
+    # We steal the RefPair class, but we do not care about the name for
+    # writing to output, use "name" as a placeholder
     ref_pair = classes.RefPair(san_ref_seq,san_gen_seq,"name")
 
-    gen_start = 'NA' # can't use False, as zero index also evaluates
+    gen_start = 'NA' # Can't use False, as zero index also evaluates
     ref_start = 'NA'
     gen_list = []
     ref_list = []
     for i, (rg,rf) in enumerate(zip(gen_seq,ref_seq)):
-        #print "%d, rg is %s, rf is %s, start is %s" % (i,rg,rf,ref_start)
+        # A gap in both genomic and reference sequences is unlikely,
+        # but we should account for it just in case
         if rf == '-' and rg == '-':
             pass
+
         # Identify inserts in reference sequence
+        # We found an indel in the ref relative to the genomic
         elif rf != '-' and rg == '-' and ref_start == 'NA':
+            # In order to maintain the reading frame, we actually need
+            # to check the codon position
             if ref_pair.index_rposition() == 1:
+                # If our gap starts in RF +1 then we can continue
                 ref_start = i
             else:
+                # If not, keep checking
                 pass
             ref_pair.incr_all_ref()
+        # We continue an indel in the reference sequence
         elif rf != '-' and rg == '-' and ref_start != 'NA':
+            # If the indel is at the end, then we need to close it
             if i == (len(ref_seq) - 1):
                 ref_end = sequence.close_gap(i,ref_pair.index_rposition())
                 ref_list.append([ref_start,ref_end])
                 ref_pair.incr_all_ref()
                 ref_start = 'NA'
             else:
+                # If not at the end, keep going
                 ref_pair.incr_all_ref()
+        # We started an indel and now we find a residue in the genomic sequence
         elif rf != '-' and rg != '-' and ref_start != 'NA':
+            # Simply close the gap
             ref_end = sequence.close_gap(i,ref_pair.index_rposition())
             ref_list.append([ref_start,ref_end])
             ref_pair.incr_all_ref()
             ref_start = 'NA'
+
         # Repeat for genomic sequence
         elif rg != '-' and rf == '-' and gen_start == 'NA':
             if ref_pair.index_gposition() == 1:
@@ -99,23 +122,31 @@ for infile in args.infiles:
             gen_start = 'NA'
 
 
+    # Check over all of the reference indices
     ref_indices = []
     for (start,stop) in ref_list:
+        # Check to make sure that indices are actually realistic
+        # They will not be added if they are less than 3 bases, if
+        # they are not multiples of three, or if they are invalid
         if sequence.check_indices(start,stop):
+            # Add all of the individual index values to another list
             sequence.expand_indices(start,stop,ref_indices)
 
+    # Check over all of the genomic indices
     gen_indices = []
     for (start,stop) in gen_list:
         if sequence.check_indices(start,stop):
-            sequence.expand_indices(start,stop,ref_indices)
+            sequence.expand_indices(start,stop,gen_indices)
 
+    # Technically, we need to remove all non-redundant ones from each
     indices = list(set(ref_indices) | set(gen_indices))
-    #print indices
 
-    new_ref_seq = sequence.trim_sequence(ref_seq,indices)#ref_indices)
-    new_gen_seq = sequence.trim_sequence(gen_seq,indices)#gen_indices)
-    new_rna_seq = sequence.trim_sequence(rna_seq,indices)#gen_indices)
+    # Finally, trim the sequences
+    new_ref_seq = sequence.trim_sequence(ref_seq,indices)
+    new_gen_seq = sequence.trim_sequence(gen_seq,indices)
+    new_rna_seq = sequence.trim_sequence(rna_seq,indices)
 
+    # Write aligned sequences
     with open(out_align,'w') as o:
         o.write(">" + ref_header + "\n")
         for chunk in strings.split_input(new_ref_seq,60):
@@ -127,6 +158,7 @@ for infile in args.infiles:
         for chunk in strings.split_input(new_rna_seq,60):
             o.write(chunk + "\n")
 
+    # Write sequences without gaps
     with open(out_seq,'w') as o2:
         o2.write(">" + ref_header + "\n")
         for chunk in strings.split_input(strings.sanitize(new_ref_seq),60):
