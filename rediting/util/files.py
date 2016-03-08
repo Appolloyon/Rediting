81cd2db3390e05ef356671185907e9ab49d78f96
Appolloyon <cklinger@ualberta.ca>
2016-03-08 12:02:28 -0700

    Added module description and comments.

--- rediting/util/files.py
+++ rediting/util/files.py
@@ -1,11 +1,13 @@
 """."""
 
+"""This module contains functions pertaining to dealing with reading from
+files and organizing the resulting data into data structures."""
 
 def nonblank_lines(f):
-    """skip blank lines"""
+    """read file lines, but skip blank lines"""
     for l in f:
         line = l.strip('\n')
-        if line:
+        if line: # Equivalent to if 'line not blank'
             yield line
 
 
@@ -14,10 +16,13 @@ def build_seqdict(infile, seqdict):
     with open(infile,'U') as f:
         for line in nonblank_lines(f):
             line = line.strip('\n')
+            # These are the header lines
             if line.startswith(">"):
                 line = line.strip(">")
                 ID = line
                 seqdict[ID] = ''
+            # If not a header line, it must be part of the sequence
+            # for the previous header, so add it to the dict value
             else:
                 seqdict[ID] += line
     return seqdict
