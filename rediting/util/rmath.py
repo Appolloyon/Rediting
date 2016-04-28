8ee8736aacf2f2e1ff3edb979609d546db2360cc
Appolloyon <cklinger@ualberta.ca>
2016-04-28 08:14:46 -0600

    Added a function to pull random values by weight.

--- rediting/util/rmath.py
+++ rediting/util/rmath.py
@@ -4,9 +4,12 @@
 includes simple calculations such as mean, but also specific calcs for
 things like pearson correlation coefficient and polyT"""
 
-
 import math
+import collections
+import random
+from bisect import bisect
 
+from classes import matrices
 from sequence import polyTpercent
 
 def calc_mean(values):
@@ -58,3 +61,50 @@ def ispolyTpercent(plist, percent):
             pass
     # This only returns False if ALL windows are False
     return False
+
+
+def calculate_entropy(aa_list):
+    """Calculates entropy for a list of values"""
+    # Calculate aa entropy without gaps
+    new_aa_list = [x for x in aa_list if x != '-']
+    num_gaps = len(aa_list) - len(new_aa_list)
+    # Determine how much of the total position is gaps
+    gap_proportion = float(num_gaps)/len(aa_list)
+    num_aa = len(new_aa_list)
+    aa_count = collections.Counter(new_aa_list)
+    total_entropy = 0
+    for k,v in aa_count.items():
+        aa_proportion = (float(v)/num_aa)
+        # Calculate normalized entropy
+        entropy = aa_proportion * math.log(aa_proportion) * (1/math.log(20))
+        total_entropy += entropy
+    total_entropy = (total_entropy * -1)
+    # End result takes into account both gaps and entropy
+    result = (1 - gap_proportion) * (1 - total_entropy)
+    return result
+
+def calculate_average_score_diff(rg,rm,aa_list):
+    """Calculates average edit score difference between
+    genomic/transcript amino acid and reference sequences"""
+    total_score = 0
+    for ref_aa in aa_list:
+        scr_before = matrices.Blosum62(rg,ref_aa).sub_score()
+        scr_after = matrices.Blosum62(rm,ref_aa).sub_score()
+        total_score += (scr_after - scr_before)
+    return (float(total_score)/len(aa_list))
+
+def weighted_choice(choices):
+    """Allows random choices but with weighted values. Takes a list of
+    2-length tuples as input. Code provided by:
+
+    http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+    """
+    values, weights = zip(*choices)
+    total = 0
+    cumulative_weight = []
+    for w in weights:
+        total += w
+        cumulative_weight.append(w)
+    rand_val = random.random() * total
+    i = bisect(cumulative_weight, rand_val)
+    return values[i]
