dbdfd4c13fce7f5f89941c8fa6e8986f35f0abaf
Appolloyon <cklinger@ualberta.ca>
2016-04-28 08:58:44 -0600

    Added functions to parse files for the weighted mutation program.

--- rediting/util/files.py
+++ rediting/util/files.py
@@ -27,3 +27,37 @@ def build_seqdict(infile, seqdict):
             else:
                 seqdict[ID] += line
     return seqdict
+
+
+def parse_codon_bias(infile, codon_weights):
+    """Builds a list of codon position bias weights"""
+    with open(infile,'U') as f:
+        for line in nonblank_lines(f):
+            llist = line.strip('\n').split(',')
+            for i,w in enumerate(llist):
+                codon_weights.append(((i+1),float(w)))
+    return codon_weights
+
+
+def parse_base_bias(infile, base_dict, base_weights):
+    """Builds a dict of lists for base conversion, and also
+    determines the relative frequency of each changing base"""
+    # Keep track of all total bases as well
+    total_freq_dict = {'A':0,'G':0,'T':0,'C':0}
+    with open(infile,'U') as f:
+        for line in nonblank_lines(f):
+            llist = line.strip('\n').split(',')
+            start_base = llist[0]
+            end_base = llist[1]
+            freq = float(llist[2])
+            # Update for each base
+            if start_base not in base_dict.keys():
+                base_dict[start_base] = [(end_base,freq)]
+            else:
+                base_dict[start_base].append((end_base,freq))
+            # Update for total bases
+            total_freq_dict[start_base] += freq
+    for k,v in total_freq_dict.items():
+        base_weights.append((k,v))
+    return base_dict,base_weights
+
